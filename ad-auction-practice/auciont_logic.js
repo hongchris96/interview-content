67ab116edbdf803204c23ca2741e2ec4cdf2ed40
hanmabaki <hongchris96@hotmail.com>
2025-06-04 21:44:33 -0700

    Solve auction logic

--- ad-auction-practice/auciont_logic.js
+++ ad-auction-practice/auciont_logic.js
@@ -2,3 +2,31 @@
 // select a combination of bids that fills a 120-second ad break
 // while maximizing total revenue.
 
+const bids = [
+    { id: 'a1', price: 50, duration: 30 },
+    { id: 'a2', price: 30, duration: 60 },
+    { id: 'a3', price: 90, duration: 60 },
+    { id: 'a4', price: 25, duration: 30 },
+    { id: 'a5', price: 60, duration: 30 }
+];
+
+const breakDuration = 120;
+
+function maxRevenue() {
+    // Greedy sort by price-per-second
+    bids.sort((a, b) => (b.price / b.duration) - (a.price / a.duration));
+
+    const selected = [];
+    let time = 0;
+
+    for (const bid of bids) {
+        if (time + bid.duration <= breakDuration) {
+            selected.push(bid);
+            time += bid.duration;
+        }
+    }
+
+    return selected;
+}
+
+console.log(maxRevenue())
